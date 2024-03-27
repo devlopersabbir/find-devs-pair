6b5024b7bd5420ac402c48229b3cf1b740aa640d
devlopersabbir <devlopersabbir@gmail.com>
2024-03-28 03:04:35 +0600

    [issue 🕹] useState conditionally render

--- src/app/(room)/_components/video-player.tsx
+++ src/app/(room)/_components/video-player.tsx
@@ -24,38 +24,53 @@ type VideoProps = {
 
 const DevFinderVideo = ({ roomId, token }: VideoProps) => {
   const session = useSession();
-  if (!session.data) return;
+  const [client, setClient] = useState<StreamVideoClient | null>(null);
+  const [call, setCall] = useState<Call | null>(null);
 
-  const user: User = {
-    id: session.data.user?.id,
-    name: session.data.user?.name!,
-    image: session.data.user?.image!,
-  };
-  console.log("user: ", user);
+  useEffect(() => {
+    if (!session.data || !roomId || !config.NEXT_PUBLIC_GET_STREAM_API_KEY)
+      return; /** Do nothing if conditions are not met */
 
-  if (!roomId || !config.NEXT_PUBLIC_GET_STREAM_API_KEY)
-    return <NotFound text="Unexpected Error" />;
-
-  const [client] = useState<StreamVideoClient>(() => {
+    const user: User = {
+      id: session.data.user?.id,
+      name: session.data.user?.name!,
+      image: session.data.user?.image!,
+    };
     const clnt = new StreamVideoClient({
       apiKey: config.NEXT_PUBLIC_GET_STREAM_API_KEY,
       user,
       token,
     });
-    return clnt;
-  }); /** client state */
-  const [call, setCall] = useState<Call | null>(null); /** call state */
+    setClient(clnt);
+
+    // Clean-up function
+    return () => {
+      clnt.disconnectUser();
+    };
+  }, [session, roomId, token]);
 
   useEffect(() => {
-    const call = client.call("default", roomId);
-    call.join({
+    if (!client || !roomId) return; /** Do nothing if conditions are not met */
+
+    const newCall = client.call("default", roomId);
+    newCall.join({
       create: true,
     });
-    setCall(call);
-  }, [roomId, client]);
+    setCall(newCall);
 
-  if (!call) return <NotFound text="Unexpected Error with call" />;
+    // Clean-up function
+    return () => {
+      newCall.endCall();
+    };
+  }, [client, roomId]);
 
+  if (!session.data || !roomId || !config.NEXT_PUBLIC_GET_STREAM_API_KEY) {
+    return <NotFound text="Unexpected Error" />;
+  }
+
+  if (!client || !call) {
+    return <NotFound text="Unexpected Error with call" />;
+  }
   return (
     <StreamVideo client={client}>
       <StreamCall call={call}>
