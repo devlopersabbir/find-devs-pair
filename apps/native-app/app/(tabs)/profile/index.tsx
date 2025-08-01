9de93330a68a30764e5acc727ad39bea6fc59d00
devlopersabbir <devlopersabbir@gmail.com>
2025-08-01 16:08:14 +0600

    [feat] profile page with profile picture shows

--- apps/native-app/app/(tabs)/profile/index.tsx
+++ apps/native-app/app/(tabs)/profile/index.tsx
@@ -1,10 +1,37 @@
+import ProfileHeader from "@/app/_components/profiles/profile-header";
+import { devs } from "@/constants";
+import { profileStyle } from "@/styles/profile.style";
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "expo-image";
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function Profile() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Profile</Text>
+    <View style={profileStyle.container}>
+      <ProfileHeader />
+      <View style={profileStyle.wrapper}>
+        <View style={profileStyle.profileView}>
+          <View style={profileStyle.imageView}>
+            <Image
+              placeholder={{ blurhash }}
+              source={devs[5].image}
+              alt={devs[5].name}
+              contentFit="cover"
+              transition={1000}
+              style={profileStyle.profileImage}
+            />
+          </View>
+          <View style={profileStyle.generel}>
+            <Ionicons name="flag" size={25} style={profileStyle.generelIcon} />
+            <Text style={profileStyle.generelText}>{devs[5].name}, </Text>
+            <Text style={profileStyle.generelText}>{devs[5].age}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
---
+++ apps/native-app/app/_components/profiles/profile-header.tsx
@@ -0,0 +1,19 @@
+import ApplicationLogo from "@/components/shared/application-logo";
+import { COLORS } from "@/constants";
+import { devStyles } from "@/styles/devs.style";
+import { Ionicons } from "@expo/vector-icons";
+import { View, TouchableOpacity } from "react-native";
+
+const ProfileHeader = () => {
+  const editProfile = () => {};
+  return (
+    <View style={devStyles.headerContainer}>
+      <Ionicons name="settings" color={COLORS.surface} size={25} />
+      <ApplicationLogo />
+      <TouchableOpacity activeOpacity={0.9} onPress={editProfile}>
+        <Ionicons name="pencil" size={25} color={COLORS.surface} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default ProfileHeader;
